03b0274a06f48ac4404ffb23465694651a10040e
luismanresa <clara.piot27@gmail.com>
2019-01-07 23:07:37 +0100

    cool à 23h

--- index.php
+++ index.php
@@ -4,42 +4,57 @@ function supp_sautlignecsv($chemin){
 
     $row = 1;
     $newtab = array();
-    $temp = "";
+    $temp ="";
+    $regex = "#[0-9]#";
+    
     if(($handle = fopen("sav.txt", "r"))!==FALSE){
         while (($data = fgetcsv($handle,1000,","))!==FALSE) {
             $num = count($data);
-            /*echo "<p> $num champs à la ligne $row: <br /></p>\n";*/
-            $row++;
+            echo "<p> $num champs à la ligne $row: <br /></p>\n";
+            
             if($row>=2){
 
                 for($c=0;$c<$num;$c++){
      
-                        
-                        
-                        if(substr($data[$c],-1)!='' ){
-                             if(substr($data[$c],-1)==','){
-                                array_push($newtab,$data[$c]);
-                             }else {
-                                $temp.=$data[$c];
-                             }  
+                        echo "<b>".$data[$c]."</b><br>";
+                        $ligne=trim($data[$c]);
+                        $dernier_caractere=substr($ligne,-1);
+                        echo "dernier caractere : ".$dernier_caractere."</br>";
+                        $condition = ($dernier_caractere=="," || $dernier_caractere=="\"" || preg_match($regex, $dernier_caractere) || $dernier_caractere=="'");
+                        if($condition && $temp==""){
+                            array_push($newtab,$ligne);
+                        }else if(!$condition && $temp==""){
+                            $temp.=$ligne." ";
+                        }else if($condition && $temp!=""){
+                            $temp.=$ligne;
+                            array_push($newtab,$temp);
+                            $temp="";
+                        }else if(!$condition && $temp!=""){
+                            $temp.=$ligne." ";
                         }
 
+
                 }
-            }else if($row==2){
+            }else if($row==1) {
+                
                 for($c=0;$c<$num;$c++){
-                    array_push($newtab,$data[$c]);
+                    echo "<b>".$data[$c]."</b><br>";
+                        $data[$c]=trim($data[$c]);
+                        echo "premier caractere : ".substr($data[$c],-1);
+                        array_push($newtab,$data[$c]);
                 }
             }
+            $row++;
         }
         var_dump($newtab);
         fclose($handle);
 
-/*        $date = date("dmY");
+        $date = date("dmY");
         if(file_exists('sav_'.$date.'.csv')){
             echo "<h1>Le fichier existe déjà supprimelez svp</h1>";
         }else{
             $fp = fopen('sav_'.$date.'.csv', 'x+');
-            var_dump($newtab);
+       
             foreach($newtab as $fields){
                 $fields=str_replace("é","e",$fields);
                 $fields=str_replace("è","e",$fields);
@@ -47,8 +62,9 @@ function supp_sautlignecsv($chemin){
                 $fields = array($fields);
                 fputcsv($fp, $fields);
             }
+            echo "<h1>Fichier genere</h1>";
             fclose($fp);
-        }*/
+        }
     }else {
         echo "pb lors de l'ouverture du fichier";
     }

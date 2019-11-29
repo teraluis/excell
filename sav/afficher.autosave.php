89ed5c97259893d959d370397c65ba72bb6185b6
luis manresa ramirez <luismanresaramirez@gmail.com>
2019-11-29 19:06:40 +0100

    interface graphique

--- sav/afficher.autosave.php
+++ sav/afficher.autosave.php
@@ -4,16 +4,14 @@ require_once('Sav.php');
 $filename ="sav.csv";
 $sav = new Sav($filename);
 $sav->generate();
-	
 try {
 	$sav->createFichier("sav");	
-	//header("Location:suces.php");
 } catch (Exception $e) {
 	echo 'Exception reçue : ',  $e->getMessage(), "\n";
 	die();
 }
-$sav->forcerTelechargement();
-echo $sav->afficherOutput();
+//$sav->forcerTelechargement();
+//echo $sav->afficherOutput();
 //sleep(60*15);
 //$sav->envoiMail("adv@angeleyes-eyewear.com");
 ?>
--- sav/telecharger.php
+++ sav/telecharger.php
@@ -10,8 +10,8 @@ try {
 	echo 'Exception reçue : ',  $e->getMessage(), "\n";
 	die();
 }
-//$sav->forcerTelechargement();
-echo $sav->afficherOutput();
+$sav->forcerTelechargement();
+//echo $sav->afficherOutput();
 //sleep(60*15);
 //$sav->envoiMail("adv@angeleyes-eyewear.com");
 ?>

d33666179ae833e07718ce536c6d2cad518d26b8
Luis Manresa <luismanresaramirez@gmail.com>
2019-12-06 10:30:31 +0100

    hacheur

--- HACHEUR/hacher.php
+++ HACHEUR/hacher.php
@@ -12,4 +12,8 @@ try {
 	die();
 }
 echo "<p>Touts les fichiers ont été correctement généres</p>";
+$nbfichiers=ceil($hacheur->getNblines()/$maxlines);
+$restants=$hacheur->getNblines() % $maxlines  ;
+echo "<br>nb fichiers : au peu près ".intval($nbfichiers)." pour ".$maxlines." maximum par fichier";
+echo "<br>sur le derinier fichier il restera maxim  ".$restants." lignes ";
 ?>
